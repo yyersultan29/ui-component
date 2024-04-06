213b2e39605e634912a31ee4a58e1608cd09a574
yyersultan29 <yersultanyerkinbek01@gmail.com>
2024-04-06 17:56:44 +0500

    box changed

--- src/components/box/box.tsx
+++ src/components/box/box.tsx
@@ -1,9 +1,13 @@
+import { FC, ReactNode } from "react"
 
+interface BoxProps {
+    children: ReactNode
+}
 
-export const Box = () => {
+export const Box:FC<BoxProps> = ({children}) => {
     return (
         <div className="rounded-lg p-5 bg-gray-100">
-            Box
+            {children}
         </div>
     )
 }
