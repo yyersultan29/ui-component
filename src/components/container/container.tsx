0559d5ed02a3c83b367ab3032dd3a4db2f3ff7bd
yyersultan29 <yersultanyerkinbek01@gmail.com>
2024-04-16 13:50:06 +0500

    done

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -5,8 +5,10 @@ interface ContainerProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 export const Container:FC<ContainerProps> = ({icon,children,...otherProps}) => {
+    const size = 24;
+    const borderRadius = 12;
     return (
-        <div {...otherProps}>
+        <div {...otherProps} id={size + ""}>
             {icon && <div>{icon}</div>}
             {children}
         </div>
