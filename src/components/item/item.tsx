bb27da4e582a9b838689e02427172dd2c33da148
Yersultan <129984720+yyersultan29@users.noreply.github.com>
2024-04-22 23:17:40 +0500

    Update item.tsx

--- src/components/item/item.tsx
+++ src/components/item/item.tsx
@@ -8,7 +8,6 @@ export const Item: FC<ItemProps> = ({children}) => {
     return (        
         <div className="p-3 border-lg bg-gray-100 ">
             {children}
-            <h1> Item changed</h1>
         </div>
     )
 }
