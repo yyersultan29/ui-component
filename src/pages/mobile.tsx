1e3e1ce64d7c63ce95ef5c1c13adc7477a84e0e2
Yersultan <129984720+yyersultan29@users.noreply.github.com>
2025-02-06 22:34:39 +0500

    done

--- src/pages/mobile.tsx
+++ src/pages/mobile.tsx
@@ -1,11 +1,3 @@
 export const ProfilePageMobile = () => {
-  return (
-    <div className="bg-red-400 p-5 ">
-      <h2>Mobile page</h2>
-      <img
-        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fartgallery.yale.edu%2Fresearch-and-learning%2Fcuratorial-areas&psig=AOvVaw0gcYWHzFE8howvlCYqe0JY&ust=1737101062960000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMDb_-_j-YoDFQAAAAAdAAAAABAE"
-        alt="img"
-      />
-    </div>
-  );
+  return <div className="bg-red-400 p-5 ">mobile page</div>;
 };
